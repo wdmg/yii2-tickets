9ddadfd5b5d5222433521de5e86cd0c2634acda3
alex-wdmg <wdmg.com.ua@gmail.com>
2019-01-03 00:04:00 +0200

    Add use dependencies

--- Module.php
+++ Module.php
@@ -2,6 +2,8 @@
 
 namespace wdmg\tickets;
 
+use Yii;
+
 /**
  * tickets module definition class
  */
@@ -18,7 +20,8 @@ class Module extends \yii\base\Module
     public function init()
     {
         parent::init();
-
-        // custom initialization code goes here
+        if (Yii::$app instanceof \yii\console\Application) {
+            $this->controllerNamespace = 'wdmg\tickets\commands';
+        }
     }
 }
