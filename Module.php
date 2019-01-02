9d2ae8ca396a23c92fdaf83b41ad7b07232172e7
alex-wdmg <wdmg.com.ua@gmail.com>
2019-01-02 21:20:21 +0200

    Added module files.

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace wdmg\tickets;
+
+/**
+ * tickets module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'wdmg\tickets\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
