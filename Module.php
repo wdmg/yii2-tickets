29041f7a00a6a0f7377c46894942238a4f0d0937
alex-wdmg <wdmg.com.ua@gmail.com>
2019-06-05 13:09:05 +0300

    Module transferred to base module interface. Update Yii2 version.

--- Module.php
+++ Module.php
@@ -6,7 +6,7 @@ namespace wdmg\tickets;
  * Yii2 Tickets
  *
  * @category        Module
- * @version         1.1.2
+ * @version         1.1.3
  * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
  * @link            https://github.com/wdmg/yii2-tickets
  * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
@@ -15,11 +15,12 @@ namespace wdmg\tickets;
  */
 
 use Yii;
+use wdmg\base\BaseModule;
 
 /**
- * tickets module definition class
+ * Tickets module definition class
  */
-class Module extends \yii\base\Module
+class Module extends BaseModule
 {
     /**
      * {@inheritdoc}
@@ -32,11 +33,6 @@ class Module extends \yii\base\Module
     public $defaultRoute = "list/all";
 
     /**
-     * @var string the prefix for routing of module
-     */
-    public $routePrefix = "admin";
-
-    /**
      * @var string, the name of module
      */
     public $name = "Tickets";
@@ -47,118 +43,17 @@ class Module extends \yii\base\Module
     public $description = "Support Ticket System";
 
     /**
-     * @var string the vendor name of module
-     */
-    private $vendor = "wdmg";
-
-    /**
      * @var string the module version
      */
-    private $version = "1.1.2";
+    private $version = "1.1.3";
 
     /**
      * @var integer, priority of initialization
      */
     private $priority = 10;
 
-    /**
-     * @var array of strings missing translations
-     */
-    public $missingTranslation;
-
-    /**
-     * {@inheritdoc}
-     */
-    public function init()
+    public function bootstrap($app)
     {
-        parent::init();
-
-        // Set controller namespace for console commands
-        if (Yii::$app instanceof \yii\console\Application)
-            $this->controllerNamespace = 'wdmg\tickets\commands';
-
-        // Set current version of module
-        $this->setVersion($this->version);
-
-        // Register translations
-        $this->registerTranslations();
-
-        // Normalize route prefix
-        $this->routePrefixNormalize();
-    }
-
-    /**
-     * Return module vendor
-     * @var string of current module vendor
-     */
-    public function getVendor() {
-        return $this->vendor;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function afterAction($action, $result)
-    {
-
-        // Log to debuf console missing translations
-        if (is_array($this->missingTranslation) && YII_ENV == 'dev')
-            Yii::warning('Missing translations: ' . var_export($this->missingTranslation, true), 'i18n');
-
-        $result = parent::afterAction($action, $result);
-        return $result;
-
-    }
-
-    // Registers translations for the module
-    public function registerTranslations()
-    {
-        Yii::$app->i18n->translations['app/modules/tickets'] = [
-            'class' => 'yii\i18n\PhpMessageSource',
-            'sourceLanguage' => 'en-US',
-            'basePath' => '@vendor/wdmg/yii2-tickets/messages',
-            'on missingTranslation' => function($event) {
-
-                if (YII_ENV == 'dev')
-                    $this->missingTranslation[] = $event->message;
-
-            },
-        ];
-
-        // Name and description translation of module
-        $this->name = Yii::t('app/modules/tickets', $this->name);
-        $this->description = Yii::t('app/modules/tickets', $this->description);
-    }
-
-    public static function t($category, $message, $params = [], $language = null)
-    {
-        return Yii::t('app/modules/tickets' . $category, $message, $params, $language);
-    }
-
-    /**
-     * Normalize route prefix
-     * @return string of current route prefix
-     */
-    public function routePrefixNormalize()
-    {
-        if(!empty($this->routePrefix)) {
-            $this->routePrefix = str_replace('/', '', $this->routePrefix);
-            $this->routePrefix = '/'.$this->routePrefix;
-            $this->routePrefix = str_replace('//', '/', $this->routePrefix);
-        }
-        return $this->routePrefix;
-    }
-
-    /**
-     * Build dashboard navigation items for NavBar
-     * @return array of current module nav items
-     */
-    public function dashboardNavItems()
-    {
-        return [
-            'label' => $this->name,
-            'url' => [$this->routePrefix . '/tickets/'],
-            'active' => in_array(\Yii::$app->controller->module->id, ['tickets'])
-        ];
+        parent::bootstrap($app);
     }
 }
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Ticket system for Yii2
 
 # Requirements 
 * PHP 5.6 or higher
-* Yii2 v.2.0.19 and newest
+* Yii2 v.2.0.20 and newest
 * [Yii2 Editor](https://github.com/wdmg/yii2-editor) module
 * [Yii2 Tasks](https://github.com/wdmg/yii2-tasks) module (optionaly)
 * [Yii2 Users](https://github.com/wdmg/yii2-users) module (optionaly)
@@ -40,12 +40,6 @@ To add a module to the project, add the following data in your configuration fil
         ...
     ],
 
-If you have connected the module not via a composer add Bootstrap section:
-
-`
-$config['bootstrap'][] = 'wdmg\tickets\Bootstrap';
-`
-
 # Routing
 Use the `Module::dashboardNavItems()` method of the module to generate a navigation items list for NavBar, like this:
 
@@ -62,5 +56,5 @@ Use the `Module::dashboardNavItems()` method of the module to generate a navigat
 
 
 # Status and version [in progress development]
+* v.1.1.3 - Module transferred to base module interface. Update Yii2 version.
 * v.1.1.2 - Bugfix, routing and refactoring
-* v.1.1.1 - Update Yii2 version
